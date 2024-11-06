a2095404ff11cb8a235705687b64f53b5d41f4ee
DmitriyBabij <132613323+DmitriyBabij@users.noreply.github.com>
2024-11-06 20:49:58 +0200

    hw

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,43 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function createBoxes(amount) {
+  const boxesContainer = document.getElementById('boxes');
+  const boxes = [];
+
+  for (let i = 0; i < amount; i++) {
+    const size = 30 + i * 10;
+    const box = document.createElement('div');
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.push(box);
+  }
+
+  boxesContainer.append(...boxes);
+}
+
+function destroyBoxes() {
+  const boxesContainer = document.getElementById('boxes');
+  boxesContainer.innerHTML = ''; 
+}
+
+const input = document.querySelector('#controls input');
+const createButton = document.querySelector('[data-create]');
+const destroyButton = document.querySelector('[data-destroy]');
+
+createButton.addEventListener('click', () => {
+  const amount = Number(input.value);
+
+  if (amount < 1 || amount > 100) {
+    alert('Please enter a number between 1 and 100.');
+    return;
+  }
+
+  destroyBoxes();
+  createBoxes(amount); 
+  input.value = '';
+});
+
+destroyButton.addEventListener('click', destroyBoxes);
